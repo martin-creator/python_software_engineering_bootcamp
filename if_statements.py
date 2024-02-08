380b6ee64344c51a9597065f6d49cdccd31bea91
martin-creator <martinlubowa@outlook.com>
2024-02-09 02:24:35 +0300

    Added information about if statements

---
+++ if_statements.py
@@ -0,0 +1,37 @@
+# if statements are used to test for particular conditions and respond appropriately.
+# They are used to control the flow of a program.
+# The if statement is used to test a condition. If the condition is true, the code block following the if statement is executed.
+# If the condition is false, the code block following the if statement is skipped and the code block following the else statement is executed.
+# The else statement is used to execute a block of code if the condition is false.
+# The elif statement is used to test multiple conditions.
+# The elif statement is used to check multiple expressions for TRUE and execute a block of code as soon as one of the conditions evaluates to TRUE.
+# The elif statement is short for "else if".
+# The else statement is used to execute a block of code if the condition is false.
+# The else statement is used to execute a block of code if the condition is false.
+
+# Example 1: Using the if statement
+x = 10
+y = 20
+
+if x < y:
+    print("x is less than y")
+
+# Output: x is less than y
+    
+# Example 2: Using the if statement
+x = 10
+y = 20
+
+if x > y:
+    print("x is greater than y")
+
+
+# Common uses of if statements in real world applications:
+# 1. To check if a user is authenticated
+# 2. To check if a user is authorized
+# 3. To check if a user is logged in
+# 4. To check if a user is registered
+# 5. To check if a user is active
+# 6. To check if a user is blocked
+# 7. To check if a user is banned
+# 8. To check if a user is suspended
