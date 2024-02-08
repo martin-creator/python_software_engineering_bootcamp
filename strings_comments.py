f4fa8066462f30921810d41e9be2a6ca443b091e
martin-creator <martinlubowa@outlook.com>
2024-02-09 02:14:22 +0300

    Added f strings and comments

---
+++ strings_comments.py
@@ -0,0 +1,39 @@
+# Types of string formating
+# String formatting is a way to format strings in Python. There are three ways to perform string formatting.
+# Formatting with the % operator
+# Formatting with the format() method
+# Formatting with f-strings
+#
+# Example 1: Formatting with the % operator
+name = "John"
+age = 23
+print("My name is %s and I am %d years old" % (name, age))    # Output: My name is John and I am 23 years old
+
+# Example 2: Formatting with the format() method
+name = "John"
+age = 23
+print("My name is {} and I am {} years old".format(name, age))    # Output: My name is John and I am 23 years old
+
+# Example 3: Formatting with f-strings
+name = "John"
+age = 23
+print(f"My name is {name} and I am {age} years old")    # Output: My name is John and I am 23 years old
+
+
+# Types of comments
+# Comments are used to explain the code. Python ignores comments, and you can use them to leave notes within the code. There are two types of comments in Python:
+# Single-line comments
+# Multi-line comments
+#
+# Single-line comments
+# Single-line comments are used to comment on a single line. You can use the hash (#) symbol to write a single-line comment.
+# Example 1: Single-line comments
+# This is a single-line comment
+# print("Hello, World!")
+
+# Example 2: Multi-line comments
+# Multi-line comments are used to comment on multiple lines. You can use triple quotes (""") to write a multi-line comment.
+# """
+# This is a multi-line comment
+# print("Hello, World!")
+# """
