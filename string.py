3974811a6c3bf3927862b5deee8d6a4008a72023
martin-creator <martinlubowa@outlook.com>
2024-02-08 19:41:02 +0300

    Added string descriptions

---
+++ string.py
@@ -0,0 +1,20 @@
+
+# String is a sequence of characters and can be accessed by index
+# Strings are immutable
+# Strings can be sliced
+# Strings can be concatenated
+# Strings can be repeated
+# Strings can be formatted
+# Strings can be compared
+# Strings can be searched
+# Strings can be replaced
+# Strings can be split
+# Strings can be joined
+# Strings can be stripped
+# Strings can be justified
+# Strings can be centered
+# Strings can be encoded
+# Strings can be decoded
+# Strings can be formatted
+
+# 
