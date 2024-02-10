7fdb060b499bbacbfe3ffaa2cbef6814e1b5610d
martin-creator <martinlubowa@outlook.com>
2024-02-10 04:51:55 +0300

    Added response validation to fastapi endpoints

--- sample_application/main.py
+++ sample_application/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+from fastapi.responses import PlainTextResponse, JSONResponse
 
 app = FastAPI()
 
-@app.get("/")
+@app.get("/", response_class=JSONResponse)
 def home():
     return {"message": "Hello, World"}
+
+@app.get("/test", response_class=PlainTextResponse)
+def test_endpoint():
+    return "Mic check 1, 2, 3..."
