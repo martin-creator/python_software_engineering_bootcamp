94e73fa0b06c2c7c89a662dbcade194a75ae725d
martin-creator <martinlubowa@outlook.com>
2024-02-10 10:58:28 +0300

    Added classes to fastapi endpoints

--- sample_application/main.py
+++ sample_application/main.py
@@ -1,7 +1,62 @@
 from fastapi import FastAPI
 from fastapi.responses import PlainTextResponse, JSONResponse
+from pydantic import BaseModel, Field
+from typing import List,Optional
 
 app = FastAPI()
+
+
+
+class User(BaseModel):
+    username: str = Field(
+        alias="name",
+        title="Username",
+        description="The name of the user",
+        max_length=50,
+        min_length=3,
+        default="John Doe"
+    )
+    age: int
+    email: str
+    liked_posts: Optional[List[int]] = Field(
+        description="The IDs of the posts that the user has liked",
+        min_items=1,
+        max_items=10
+    )
+
+    class Config:
+        max_anystr_length = 50
+
+
+class FullUserProfile(BaseModel):
+    short_bio: str
+    long_bio: str
+
+
+def get_user_info()-> (str,str):
+    user_content = {
+        "name": "John Doe",
+        "age": 25,
+        "email": "martin@gmail.com",
+        "liked_posts": [1,2,3],
+        "profile_info": profile_info
+    }
+
+    profile_info = {
+        "short_bio": "I am a software developer",
+        "long_bio": "I am a software developer with 5 years of experience. I have worked on various projects and have experience in a wide range of technologies."
+    }
+
+    user = User(**user_content)
+    full_user_profile = {
+        **user.dict(),
+        **profile_info
+    }
+
+
+    return  FullUserProfile(**full_user_profile)
+
+
 
 @app.get("/", response_class=JSONResponse)
 def home():
@@ -10,3 +65,9 @@ def home():
 @app.get("/test", response_class=PlainTextResponse)
 def test_endpoint():
     return "Mic check 1, 2, 3..."
+
+@app.post("/user", response_model=User)
+def create_user(user: User):
+    user = get_user_info()
+    return user
+    
