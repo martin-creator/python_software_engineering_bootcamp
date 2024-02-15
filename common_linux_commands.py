e8e83c49ead001216d51d1a13fbd057b3de32264
martin-creator <martinlubowa@outlook.com>
2024-02-15 09:16:57 +0300

    Common linux commands

---
+++ common_linux_commands.py
@@ -0,0 +1,113 @@
+# Common Linux commands
+
+# 1. List all files in a directory
+# The following command lists all files in a directory using the ls command.
+#
+# ls
+
+# 2. List all files in a directory with details
+# The following command lists all files in a directory with details using the ls -l command.
+#
+# ls -l
+
+# 3. Change directory
+# The following command changes the current directory to the specified directory using the cd command.
+#
+# cd directory_name
+
+
+# 4. Create a new directory
+# The following command creates a new directory using the mkdir command.
+#
+# mkdir directory_name
+
+# 5. Remove a directory
+# The following command removes a directory using the rmdir command.
+#
+# rmdir directory_name
+
+# 6. Copy a file
+# The following command copies a file using the cp command.
+#
+# cp source_file destination_file
+
+# 7. Move a file
+# The following command moves a file using the mv command.
+#
+# mv source_file destination_file
+
+# 8. Remove a file
+# The following command removes a file using the rm command.
+#
+# rm file_name
+
+# 9. View the contents of a file
+# The following command views the contents of a file using the cat command.
+#
+# cat file_name
+
+# 10. View the contents of a file with line numbers
+# The following command views the contents of a file with line numbers using the cat -n command.
+#
+# cat -n file_name
+
+# 11. View the contents of a file with pagination
+# The following command views the contents of a file with pagination using the less command.
+#
+# less file_name
+
+# 12. View the contents of a file with pagination and line numbers
+# The following command views the contents of a file with pagination and line numbers using the less -N command.
+#
+# less -N file_name
+
+# 13. View the contents of a file with pagination and search
+# The following command views the contents of a file with pagination and search using the less +/search_string command.
+#
+# less +/search_string file_name
+
+# 14. View the contents of a file with pagination and search (case insensitive)
+# The following command views the contents of a file with pagination and search (case insensitive) using the less -i +/search_string command.
+#
+# less -i +/search_string file_name
+
+# 15. Rename a file
+# The following command renames a file using the mv command.
+#
+# mv old_file_name new_file_name
+
+# 16. View the current working directory
+# The following command views the current working directory using the pwd command.
+#
+# pwd
+
+# 17. View the contents of a directory
+# The following command views the contents of a directory using the ls command.
+#
+# ls directory_name
+
+# 18. Change the permissions of a file
+# The following command changes the permissions of a file using the chmod command.
+#
+# chmod permissions file_name
+
+# 19. Change the owner of a file
+# The following command changes the owner of a file using the chown command.
+#
+# chown new_owner file_name
+
+# 20. Change the group of a file
+# The following command changes the group of a file using the chgrp command.
+#
+# chgrp new_group file_name
+
+# 21. Trace the route to a host
+# The following command traces the route to a host using the traceroute command.
+#
+# traceroute host_name
+
+# 22. View the manual page of a command
+# The following command views the manual page of a command using
+#
+# man command_name
+
